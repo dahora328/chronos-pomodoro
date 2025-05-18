7616a5817d5398a7fd183937e14d9f7a536f4971
Dahora <rodrigoliveira328@hotmail.com>
2025-05-18 16:32:55 -0300

    Update new archives

---
+++ src/assets/components/Heading.tsx
@@ -0,0 +1,8 @@
+import sytles from './Heading.module.css';
+
+type HeadingProps = {
+  children: React.ReactNode;
+};
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={sytles.heading}>{children}</h1>;
+}
