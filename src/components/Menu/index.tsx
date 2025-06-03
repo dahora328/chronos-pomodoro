bd1772e9344ea149a3a97eaad3de5cd080d307c2
Dahora <rodrigoliveira328@hotmail.com>
2025-06-02 21:40:37 -0300

    useState and useEffect, theme dark and light

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,76 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import sytles from './styles.module.css';
+import { useState, useEffect } from 'react';
+
+type AvailableTheme = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableTheme>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableTheme) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); // Não segue o link
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={sytles.menu}>
-      <a className={sytles.menuLink} href='#'>
+      <a
+        className={sytles.menuLink}
+        href='#'
+        aria-label='Ir para Home'
+        title='Ir para Home'
+      >
         <HouseIcon />
       </a>
-      <a className={sytles.menuLink} href='#'>
+      <a
+        className={sytles.menuLink}
+        href='#'
+        aria-label='Ver Histórico'
+        title='Ver Histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={sytles.menuLink} href='#'>
+      <a
+        className={sytles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={sytles.menuLink} href='#'>
-        <SunIcon />
+      <a
+        className={sytles.menuLink}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </nav>
   );
